6960c2484f62c72658505c52cae9230b8a7c55eb
YashikaBisht1 <146502783+YashikaBisht1@users.noreply.github.com>
2024-07-24 10:02:25 +0530

    Create osslab1.cpp

---
+++ osslab1.cpp
@@ -0,0 +1,38 @@
+q1)
+#include <stdio.h>
+int main(){
+  int n,i;
+  float sum[100],sum=0.0,avg=0;
+  prinf("enter the number of elements: ");
+  scanf("%d",&n);
+  for (i = 0; i < n; ++i) {
+        printf("%d. Enter number: ", i + 1);
+        scanf("%f", &num[i]);
+        sum += num[i];
+    }
+  avg=sum/n;
+  printf("Average = %.2f", avg);
+  return 0;
+}
+q2)
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char* str) {
+    int len = strlen(str);
+    for (int i = 0, j = len - 1; i < j; i++, j--) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
